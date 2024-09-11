064b0252e84eb9902ef26573d590dd6128c9e0ff
unknown <zenagsuma5@gmail.com>
2024-09-11 09:50:20 -0300

    algunos cambios

--- src/models/schemas/user.schemas.js
+++ src/models/schemas/user.schemas.js
@@ -1,5 +1,5 @@
 import { body } from "express-validator";
-import { User } from "../models/user.model.js";
+import { User } from "../user.model.js";
 
 export const userSchema = [
   body("username")
--- src/routes/all.routes.js
+++ src/routes/all.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 export const router = Router();
-
 import { ctrl } from "../controllers/controllers.js";
+import { userSchema } from "../models/schemas/user.schemas.js";
 
-router.post("/User", ctrl.createUser);
+router.post("/User", userSchema, ctrl.createUser);
 router.get("/Users", ctrl.getUsers);
 router.get("/User/:id", ctrl.getUserById);
 router.put("/User/:id", ctrl.updateUserById);
