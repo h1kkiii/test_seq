f9d0fa985e6ce8fdc787a69c160f907f9d0824d0
unknown <zenagsuma5@gmail.com>
2024-09-11 10:11:18 -0300

    se añadio el middleware a las rutas

--- src/routes/all.routes.js
+++ src/routes/all.routes.js
@@ -2,8 +2,9 @@ import { Router } from "express";
 export const router = Router();
 import { ctrl } from "../controllers/controllers.js";
 import { userSchema } from "../models/schemas/user.schemas.js";
+import { validator } from "../middlewares/validator.js";
 
-router.post("/User", userSchema, ctrl.createUser);
+router.post("/User", userSchema, validator, ctrl.createUser);
 router.get("/Users", ctrl.getUsers);
 router.get("/User/:id", ctrl.getUserById);
 router.put("/User/:id", ctrl.updateUserById);
